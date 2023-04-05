52bbc2bc8bcea5c8a2571bf1b220b58ebe4731be
mahdi rezainiya <mahdi.rezainiya@gmail.com>
2023-04-05 18:06:00 +0430

    edited to better

--- Expanding cards/index.js
+++ Expanding cards/index.js
@@ -1,16 +1,15 @@
 const panels = document.querySelectorAll('.panel');
-panels.forEach(function(panel){
-    panel.addEventListener('click' , function(e){
+panels.forEach(panel => {
+    panel.addEventListener('click' , e =>{
         removeActiveClasses();
         panel.classList.add('active');
-    panel.addEventListener('dblclick' , function(e){
+    })
+    panel.addEventListener('dblclick' , e =>
         removeActiveClasses()
-    })
-    })
+    )
 })
 function removeActiveClasses(){
-    panels.forEach(function(panel){
-        panel.classList.remove('active');
-
-    })
+    panels.forEach(panel => 
+        panel.classList.remove('active')
+    )
 }
