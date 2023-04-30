38cae58cf26db32b8998c1489105d65c0d4183d9
mahdi rezainiya <mahdi.rezainiya@gmail.com>
2023-04-30 10:38:38 +0430

    a little change to better

--- Progress Steps/index.js
+++ Progress Steps/index.js
@@ -5,50 +5,78 @@ const circles = document.querySelectorAll('.circle');
 let currentActive = 1 ;
 
 next.addEventListener('click' , e => {
-    currentActive ++ ;
+    currentActive++ ;
     if(currentActive > circles.length){
-        currentActive = circles.length ;
-    }
+        currentActive = circles.length}
     update();
 })
 
 prev.addEventListener('click' , e => {
     currentActive-- ;
     if(currentActive < 1){
-        currentActive = 1 ;
-    }
-    update();
+        currentActive = 1 ;}
+        update();
 })
 
 function update(){
-    circles.forEach((circle , idx)=>{
+    circles.forEach((circle , idx) => {
         if(idx < currentActive){
             circle.classList.add('active')
         }
-        else{
-            circle.classList.remove('active')
-        }
+        else{circle.classList.remove('active')}
     })
     // for(let [idx , circle] of circles.entries()){
     //     if(idx < currentActive){
-    //         circle.classList.add('active');
+    //         circle.classList.add('active')
     //     }
     //     else{
     //         circle.classList.remove('active')
     //     }
     // }
-
     const actives = document.querySelectorAll('.active');
-    progress.style.width = (actives.length -1) / (circles.length -1) * 100 + '%';
+    progress.style.width = (actives.length - 1) / (circles.length - 1) *100 + '%';
 
     if(currentActive === 1){
         prev.disabled = true ;
     }
-    else if(currentActive === 4){
+    else if (currentActive === 4){
         next.disabled = true ;
     }
     else{
         prev.disabled = false ;
         next.disabled = false ;
     }
+
+
 }
+
+// function update(){
+//     circles.forEach((circle , idx)=>{
+//         if(idx < currentActive){
+//             circle.classList.add('active')
+//         }
+//         else{
+//             circle.classList.remove('active')
+//         }
+//     })
+//     // for(let [idx , circle] of circles.entries()){
+//     //     if(idx < currentActive){
+//     //         circle.classList.add('active');
+//     //     }
+//     //     else{
+//     //         circle.classList.remove('active')
+//     //     }
+//     // }
+//     const actives = document.querySelectorAll('.active');
+//     progress.style.width = (actives.length -1) / (circles.length -1) * 100 + '%';
+//     if(currentActive === 1){
+//         prev.disabled = true ;
+//     }
+//     else if(currentActive === 4){
+//         next.disabled = true ;
+//     }
+//     else{
+//         prev.disabled = false ;
+//         next.disabled = false ;
+//     }
+// }
