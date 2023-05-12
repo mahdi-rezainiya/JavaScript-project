090492d71a97fe517ecd9a69c97c0b0bec69d7d5
mahdi rezainiya <mahdi.rezainiya@gmail.com>
2023-05-12 11:40:54 +0430

    changed to better

--- Scroll-animation/javascript.js
+++ Scroll-animation/javascript.js
@@ -1,16 +1,18 @@
+// select all of the boxes
 const boxes = document.querySelectorAll('.box');
-
+// add event for whole of the window
 window.addEventListener('scroll' , showBox);
-
+// external function is showBox
 function showBox(){
-    const triggerBottom = window.innerHeight / 1.2;
-    console.log(triggerBottom);
-    
+    // triggerBottom here is for measure the internal height of page in ease scroll
+    const triggerBottom = window.innerHeight / 1.2 ;
+    // console.log(triggerBottom);
     boxes.forEach(box => {
+        // 
         const boxTop = box.getBoundingClientRect().top ;
         // console.log(boxTop);
         if(boxTop < triggerBottom){
-            box.classList.add('show')
+            box.classList.add('show');
         }
         else{
             box.classList.remove('show')
